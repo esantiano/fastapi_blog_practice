7df36fdcc98f8d69adda109701f70e22381760fc
esantiano <103700795+esantiano@users.noreply.github.com>
2025-11-12 14:43:32 -0500

    set up database connections

---
+++ backend/db/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings 
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print("Database url is", SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# using sqlite
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
+
+SESSIONLOCAL = sessionmaker(autoflush=False, autocommit=False, bind=engine)
