969b22b404f2582c4925e1e78d613bb42eb81da3
esantiano <103700795+esantiano@users.noreply.github.com>
2025-11-14 15:58:39 -0500

    set up home page and blog related pages

---
+++ backend/apps/v1/route_blog.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from fastapi import Request, Depends
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from db.repository.blog import list_blogs, retrieve_blog
+from db.session import get_db
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter()
+
+@router.get("/")
+def home(request: Request, db: Session=Depends(get_db)):
+    blogs = list_blogs(db=db)
+    print(dir(request))
+    context = {"request":request, "blogs":blogs}
+    return templates.TemplateResponse("blogs/home.html", context=context)
+
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session=Depends(get_db)):
+    blog=retrieve_blog(id=id, db=db)
+    return templates.TemplateResponse("blogs/detail.html",{"request":request,"blog":blog})
--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,15 @@ from core.config import settings
 from db.session import engine
 # from db.base_class import Base
 from apis.base import api_router
+from apps.base import app_router
+from fastapi.staticfiles import StaticFiles
 
 def include_router(app): # include routes within the app
     app.include_router(api_router)
+    app.include_router(app_router)
 
+def configure_static_files(app):
+    app.mount("/static", StaticFiles(directory="static"),name="static")
 # def create_tables():
 #     Base.metadata.create_all(bind=engine)
 
@@ -14,10 +19,11 @@ def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
     # create_tables() experiment to create tables without using alembic
     include_router(app) # include the routes  
+    configure_static_files(app)
     return app
 
 app = start_application()
 
-@app.get("/")
-def hello():
-    return {"msg":"Hello FastAPI"}
+# @app.get("/")
+# def hello():
+#     return {"msg":"Hello FastAPI"}
