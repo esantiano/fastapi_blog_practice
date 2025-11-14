43ccc1d0cf0977d98958d0f2e3e773823164ddd8
esantiano <103700795+esantiano@users.noreply.github.com>
2025-11-14 11:35:57 -0500

    unit tests

---
+++ backend/tests/conftest.py
@@ -0,0 +1,76 @@
+from typing import Any
+from typing import Generator
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+#this is to include backend dir in sys.path so that we can import from db,main.py
+# helps pytest understnd where main.py file is 
+
+from db.base import Base
+from db.session import get_db
+from apis.base import api_router
+
+
+def start_application(): 
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+# Use connect_args parameter only with sqlite
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# fixtures are fns run before each function they are applied to
+# they run data to the test, db connections and app instances, 
+# returns data for each and every test 
+@pytest.fixture(scope="function") # scoped to functions 
+def app() -> Generator[FastAPI, Any, None]:
+    """
+    Create a fresh database on each test case.
+    """
+    Base.metadata.create_all(engine)  # Create the tables.
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine) # drop all test tables 
+
+
+@pytest.fixture(scope="function") 
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session  # use the session in tests.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def client(
+    app: FastAPI, db_session: SessionTesting
+) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db 
+    with TestClient(app=app) as client:
+        yield client
---
+++ backend/tests/test_routes/test_user.py
@@ -0,0 +1,7 @@
+# naming test functions should be chosen carefully 
+def test_create_user(client): # client is a fixture
+    data = {"email":"testuser@nofoobar.com","password":"testing"}
+    response = client.post("/users/",json=data)
+    assert response.status_code == 201
+    assert response.json()["email"] == "testuser@nofoobar.com"
+    assert response.json()["is_active"] == True
